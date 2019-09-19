c69592eb04bf492bc154c252bb37c33cedf513de
毛崇鑫 <maochongxin@gmail.com>
2019-09-19 20:08:58 +0800

    makefile construct allocator

---
+++ allocator.h
@@ -0,0 +1,13 @@
+//
+// Created by 毛崇鑫 on 2019/9/19.
+//
+
+#ifndef STL_ALLOCATOR_H
+#define STL_ALLOCATOR_H
+
+#include <cassert>
+#include <new>
+
+#include "alloc.h"
+#include "construct.h"
+#endif //STL_ALLOCATOR_H
---
+++ construct.h
@@ -0,0 +1,8 @@
+//
+// Created by 毛崇鑫 on 2019/9/19.
+//
+
+#ifndef STL_CONSTRUCT_H
+#define STL_CONSTRUCT_H
+
+#endif //STL_CONSTRUCT_H
