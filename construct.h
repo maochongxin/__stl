5652e5528fb32f7830a96674576e86b23baeff23
毛崇鑫 <maochongxin@gmail.com>
2019-09-19 21:57:39 +0800

    add construct0.0.1

--- construct.h
+++ construct.h
@@ -5,4 +5,32 @@
 #ifndef STL_CONSTRUCT_H
 #define STL_CONSTRUCT_H
 
+#include <new>
+#include "typetratis.h"
+
+namespace STD {
+	template <typename T1, typename T2>
+	inline void construct(T1* ptr1, const T2& value) {
+		new(ptr1) T1(value);
+	}
+	template <typename T>
+	inline void destroy(T* ptr) {
+		ptr->~T();
+	}
+
+	template <typename ForwardIterator>
+	inline void _destroy(ForwardIterator first, ForwardIterator last, _true_type) {}
+
+	template <typename ForwardIterator>
+	inline void _destroy(ForwardIterator first, ForwardIterator last, _false_type) {
+		for (; first != last; ++first) {
+			destory(&*first);
+		}
+	}
+	template <typename ForwardIterator>
+	inline void destroy(ForwardIterator first, ForwardIterator last) {
+		using is_POD_type = typename _type_traits<ForwardIterator>::is_POD_type;
+		_destroy(first, last, is_POD_type());
+	}
+}
 #endif //STL_CONSTRUCT_H
