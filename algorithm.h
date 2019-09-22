7a23e4b69fdd01306c6d76e6e4e3702c492a7d4d
毛崇鑫 <maochongxin@gmail.com>
2019-09-23 01:42:49 +0800

    update algorithm

--- algorithm.h
+++ algorithm.h
@@ -308,8 +308,216 @@ namespace STD {
 	        	}
 				return STD::make_pair(first1, first2);
 	        }
+	template <typename InputIterator1, typename InputIterator2, typename BinaryPredicate>
+	STD::pair<InputIterator1, InputIterator2>
+	        mismatch (InputIterator1 first1, InputIterator1 last1,
+	        		InputIterator2 first2, InputIterator2 last2, BinaryPredicate pred) {
+	        	for (; first1 != last1; ++first1, ++first2) {
+	        		if (!pred(*first1, *first2)) { break; }
+	        	}
+	        	return STD::make_pair(first1, first2);
+	        }
 
+	// equal
+	template <typename InputIterator1, typename InputIterator2>
+	bool equal (InputIterator1 first1, InputIterator1 last1,
+			InputIterator2 first2) {
+		return STD::equal(first1, last1, STD::equal_to<typename STD::iterator_traits<InputIterator1>::value_type>());
+	}
+	template <typename InputIterator1, typename InputIterator2, typename BinaryPredicate>
+	bool equal(InputIterator1 first1, InputIterator1 last1,
+			InputIterator2 first2, BinaryPredicate pred) {
+		for (; first1 != last1; ++first1, ++first2) {
+			if (!pred(*first1, *first2)) { return false; }
+		}
+		return true;
+	}
 
+	// is_permutation
+	template <typename ForwardIterator1, typename ForwardIterator2>
+	bool is_permutation (ForwardIterator1 first1, ForwardIterator1 last1,
+			ForwardIterator2 first2) {
+		return STD::is_permutation(first1, last1, first2,
+				STD::equal_to<typename STD::iterator_traits<ForwardIterator1>::value_type>());
+	}
+	template <typename ForwardIterator1, typename ForwardIterator2, typename BinaryPredicate>
+	bool is_permutation (ForwardIterator1 first1, ForwardIterator1 last1,
+			ForwardIterator2 first2, BinaryPredicate pred) {
+		auto res = STD::mismatch(first1, last1, first2, pred);
+		first1 = res.first;
+		first2 = res.second;
+		auto last2 =first2;
+		STD::advance(last2, STD::distance(first1, last1));
+		for (auto it1 = first1; it1 != last1; ++it1) {
+			if (STD::find_if(first1, it1, [&](decltype(*first1) val) {
+				return pred(val, *it1);
+			}) == it1) {
+				auto n = STD::count(first2, last2, *it1);
+				if (n == 0 || STD::count(it1, last1, *it) != n) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	// search
+	template <typename ForwardIterator1, typename ForwardIterator2>
+	ForwardIterator1 search (ForwardIterator1 first1, ForwardIterator1 last1,
+			ForwardIterator2 first2, ForwardIterator2 last2) {
+		return STD::search(first1, last1, first2, last2,
+				STD::equal_to<typename STD::iterator_traits<ForwardIterator1>::value_type>());
+	}
+	template <typename ForwardIterator1, typename ForwardIterator2, typename BinaryPredicate>
+	ForwardIterator1 search (ForwardIterator1 first1, ForwardIterator1 last1,
+			ForwardIterator2 first2, ForwardIterator2 last2, BinaryPredicate pred) {
+		while (first1 != last1) {
+			auto it1 = first1;
+			auto it2 = first2;
+			while (it1 != last1 && it2 != last2) {
+				if (pred(*it1, *it2)) {
+					++it1, ++it2;
+				} else {
+					break;
+				}
+			}
+			if (it2 == last2) { return first1; }
+			if (it1 == last1) { return last1; }
+			++first1;
+		}
+		return last1;
+	}
+	// advance
+	namespace {
+		template <typename InputIterator, typename Distance>
+		void _advance (InputIterator& it, Distance n, input_iterator_tag) {
+			assert(n >= 0);
+			while (n--) { ++it; }
+		}
+
+		template <typename BidirectionIterator, typename Distance>
+		void _advance (BidirectionIterator& it, Distance n, bidirectional_iterator_tag) {
+			if (n < 0) {
+				while (n++) { --it; }
+			} else {
+				while (n--) { ++it; }
+			}
+		}
+		template <typename RandomAccessIterator, typename Distance>
+		void _advance (RandomAccessIterator& it, Distance n, random_access_iterator_tag) {
+			if (n < 0) { it -= (-n); }
+			else { it += n; }
+		}
+	}
+	template <typename InputIterator, typename Distance>
+	void advance (InputIterator& it, Distance n) {
+		using iterator_category = typename STD::iterator_traits<InputIterator>::iterator_category;
+		STD::_advance(it, n, iterator_category());
+	}
+
+	namespace {
+		template <typename RandomAccessIterator, typename BinaryPredicate>
+		typename STD::iterator_traits<RandomAccessIterator>::value_type
+		mid3 (RandomAccessIterator first, RandomAccessIterator last, BinaryPredicate pred) {
+			auto mid = first + (last + 1 - first) / 2;
+			if (pred(*mid, *first)) { STD::swap(*mid, *first); }
+			if (pred(*last, *mid)) { STD::swap(*last, *mid); }
+			if (pred(*last, *first)) { STD::swap(*last, *first); }
+			auto res = *mid;
+			STD::swap(*mid, *(last - 1));
+			return res;
+		}
+		template <typename RandomAccessIterator, typename BinaryPredicate>
+		void bubble_sort(RandomAccessIterator first, RandomAccessIterator last, BinaryPredicate pred) {
+			auto len = last - first;
+			for (auto i = len; i != 0; --i) {
+				bool swaped = false;
+				for (auto p = first; p != (first + i - 1); ++p) {
+					if (pred(*(p + 1), *p)) {
+						STD::swap(*(p + 1), *p);
+						swaped = true;
+					}
+				}
+				if (!swaped) { break; }
+			}
+		}
+		template <typename RandomAccessIterator>
+		void sort(RandomAccessIterator first, RandomAccessIterator last) {
+			return sort(first, last, less<typename STD::iterator_traits<RandomAccessIterator>::value_type>());
+		}
+		template <typename RandomAccessIterator, typename BinaryPredicate>
+		void sort(RandomAccessIterator first, RandomAccessIterator last, BinaryPredicate pred) {
+			if (first >= last || first + 1 == last) { return; }
+			if (last - first <= 20) {
+				return bubble_sort(first, last, pred);
+			}
+			auto mid = mid3(first, last, pred);
+			auto p1 = first, p2 = last - 2;
+			while (p1 < p2) {
+				while (pred(*p1, mid) && (p1 < p2)) { ++p1; }
+				while (!pred(*p2, mid) && (p1 < p2)) { --p2; }
+				if (p1 < p2) { STD::swap(*p1, *p2); }
+			}
+			swap(*p1, *(last - 2));
+			sort(first, p1, pred);
+			sort(p1 + 1, last, pred);
+		}
+
+		// generate
+		template <typename InputIterator, typename Function>
+		void generate (InputIterator first, InputIterator last, Function func) {
+			for (; first != last; ++first) {
+				*first = func();
+			}
+		}
+
+		// generate_n
+		template <typename OutputIterator, typename Size, typename Generator>
+		void generate_n (OutputIterator first, Size n, Generator gen) {
+			while (n--) {
+				*first = gen();
+				++first;
+			}
+		}
+		// distance
+		template <typename InputIterator>
+		typename STD::iterator_traits<InputIterator>::difference_type
+		_distance (InputIterator first, InputIterator last, input_iterator_tag) {
+			typename STD::iterator_traits<InputIterator>::difference_type dist = 0;
+			while (first++ != last) { ++dist; }
+			return dist;
+		}
+		template <typename RandomAccessIterator>
+		typename STD::iterator_traits<RandomAccessIterator>::difference_type
+		_distance (RandomAccessIterator first, RandomAccessIterator last, random_access_iterator_tag) {
+			auto dist = last - first;
+			return dist;
+		}
+		template <typename Iterator>
+		typename STD::iterator_traits<Iterator>::difference_type
+		distance(Iterator first, Iterator last) {
+			using iterator_category = typename STD::iterator_traits<Iterator>::iterator_category;
+			return _distance(first, last, iterator_category());
+		}
+
+		// copy
+		template <typename InputIterator, typename OutputIterator>
+		OutputIterator __copy(InputIterator first, InputIterator last, OutputIterator result, STD::_true_type) {
+			auto dist = distance(first, last);
+			memcpy(result, first, sizeof(*first) * dist);
+			advance(result, dist);
+			return result;
+		}
+		template <typename InputIterator, typename OutputIterator>
+		OutputIterator __copy(InputIterator first, InputIterator last, OutputIterator result, STD::_false_type) {
+			while (first != last) { *result++ = *first++; }
+			return result;
+		}
+		template <typename InputIterator, typename OutputIterator, typename T>
+		OutputIterator _copy(InputIterator first, InputIterator last, OutputIterator result, T*) {
+			using is_pod = typename STD::_type_traits<T>::is_POD_type;
+			return __copy(first, last, result, is_pod());
+		}
+	}
 
 }
 
